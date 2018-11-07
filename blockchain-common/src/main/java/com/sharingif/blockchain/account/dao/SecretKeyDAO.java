479cdaa5352b5c9f71d883388603485b2d94d3ae
JolyHuang <JolyEmail@Gmail.COM>
2018-11-07 17:36:01 +0800

    init

---
+++ blockchain-common/src/main/java/com/sharingif/blockchain/account/dao/SecretKeyDAO.java
@@ -0,0 +1,10 @@
+package com.sharingif.blockchain.account.dao;
+
+
+import com.sharingif.blockchain.common.dao.BaseDAO;
+import com.sharingif.blockchain.crypto.model.entity.SecretKey;
+
+
+public interface SecretKeyDAO extends BaseDAO<SecretKey,java.lang.String> {
+
+}
---
+++ blockchain-common/src/main/java/com/sharingif/blockchain/account/model/entity/AddressListener.java
@@ -0,0 +1,53 @@
+package com.sharingif.blockchain.account.model.entity;
+
+
+import com.sharingif.cube.components.monitor.IObjectDateOperationHistory;
+
+import java.util.Date;
+
+public class AddressListener implements java.io.Serializable, IObjectDateOperationHistory {
+	
+	//columns START
+    /**
+     * 地址			db_column: ADDRESS 
+     */	
+	private java.lang.String address;
+    /**
+     * 创建时间			db_column: CREATE_TIME 
+     */	
+	private Date createTime;
+    /**
+     * 修改时间			db_column: MODIFY_TIME 
+     */	
+	private Date modifyTime;
+	//columns END
+
+	public void setAddress(java.lang.String address) {
+		this.address = address;
+	}
+	public java.lang.String getAddress() {
+		return this.address;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getCreateTime() {
+		return this.createTime;
+	}
+	public void setModifyTime(Date modifyTime) {
+		this.modifyTime = modifyTime;
+	}
+	public Date getModifyTime() {
+		return this.modifyTime;
+	}
+
+	public String toString() {
+		return new StringBuilder("AddressListener [")
+			.append("Address=").append(getAddress()).append(", ")
+					.append("CreateTime=").append(getCreateTime()).append(", ")
+					.append("ModifyTime=").append(getModifyTime())
+		.append("]").toString();
+	}
+	
+}
+
