86d0e5eb807b571d85916e8e2f9a425d633f9fce
JolyHuang <JolyEmail@Gmail.COM>
2019-01-15 16:56:57 +0800

    init

--- ether-observe/src/main/java/com/sharingif/blockchain/ether/block/scheduled/TransactionBusinessAccountScheduled.java
+++ ether-observe/src/main/java/com/sharingif/blockchain/ether/block/scheduled/TransactionBusinessAccountScheduled.java
@@ -2,10 +2,14 @@ package com.sharingif.blockchain.ether.block.scheduled;
 
 import com.sharingif.cube.batch.core.handler.SimpleDispatcherHandler;
 import com.sharingif.cube.batch.core.request.JobRequest;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
+@Component
+@EnableScheduling
 public class TransactionBusinessAccountScheduled {
 
     private SimpleDispatcherHandler simpleDispatcherHandler;
