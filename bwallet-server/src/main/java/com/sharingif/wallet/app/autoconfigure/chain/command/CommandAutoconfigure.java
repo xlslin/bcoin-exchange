105ba534b20effb30e3fc484959a3cf94dbb75d3
JolyHuang <JolyEmail@Gmail.COM>
2019-01-21 12:41:47 +0800

    init

--- bwallet-server/src/main/java/com/sharingif/wallet/app/autoconfigure/chain/command/CommandAutoconfigure.java
+++ bwallet-server/src/main/java/com/sharingif/wallet/app/autoconfigure/chain/command/CommandAutoconfigure.java
@@ -1,22 +1,11 @@
 package com.sharingif.wallet.app.autoconfigure.chain.command;
 
-import com.sharingif.cube.security.confidentiality.encrypt.TextEncryptor;
-import com.sharingif.wallet.app.chain.command.HttpRequestPasswordEncryptorCommand;
-import com.sharingif.wallet.app.chain.command.OleMerchantVerifySignCommand;
 import com.sharingif.wallet.app.chain.command.UserLoginRetunObjectCommand;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommandAutoconfigure {
-
-    @Bean("httpRequestPasswordEncryptorCommand")
-    public HttpRequestPasswordEncryptorCommand createHttpRequestPasswordEncryptorCommand(TextEncryptor bcryptTextEncryptor) {
-        HttpRequestPasswordEncryptorCommand httpRequestPasswordEncryptorCommand = new HttpRequestPasswordEncryptorCommand();
-        httpRequestPasswordEncryptorCommand.setTextEncryptor(bcryptTextEncryptor);
-
-        return httpRequestPasswordEncryptorCommand;
-    }
 
     @Bean("userLoginRetunObjectCommand")
     public UserLoginRetunObjectCommand createUserLoginRetunObjectCommand() {
